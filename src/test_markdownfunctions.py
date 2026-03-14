486a6f9e914251b2f068c05981ef36b1422aed35
valentijndb <valentijndb@gmail.com>
2026-03-14 13:01:02 +0100

    regex

--- src/test_markdownfunctions.py
+++ src/test_markdownfunctions.py
@@ -1,5 +1,5 @@
 import unittest
-from src.markdownfunctions import split_nodes_delimiter
+from src.markdownfunctions import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 
 from textnode import TextNode, TextType
 
@@ -83,6 +83,12 @@ class TestInlineMarkdown(unittest.TestCase):
             ],
             new_nodes,
         )
+    
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
 
 
 if __name__ == "__main__":
