ed8288dae5feb0cedb7e17549810ad40803de74c
valentijndb <valentijndb@gmail.com>
2026-03-20 20:30:26 +0100

    generatin content

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,6 +1,16 @@
 import os
 from markdownfunctions import markdown_to_html_node
+from pathlib import Path
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
 
 def generate_page(from_path, template_path, dest_path):
     print(f" * {from_path} {template_path} -> {dest_path}")
